b94f049c0f2d1531b67883645e967de6914a64a0
David <davidandradz7@gmail.com>
2026-06-30 09:02:02 -0500

    feat: Inicializacion del proyecto

---
+++ src/main/java/ec/eeasa/firmaec/FirmaecBackendApplication.java
@@ -0,0 +1,13 @@
+package ec.eeasa.firmaec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmaecBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmaecBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/eeasa/firmaec/FirmaecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.eeasa.firmaec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmaecBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
